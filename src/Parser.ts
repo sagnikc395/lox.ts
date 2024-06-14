6880f1397ff899359ecc59a576b9e8b9a356d7af
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-06-15 04:03:58 +0530

    src: start work on parser

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,16 +1,44 @@
 import type { Token } from "./Token";
+import { TokenType } from "./TokenType";
+
+type Expr = {};
 
 class Parser {
   private tokens: Array<Token>;
   private current: number = 0;
-
 
   constructor(tokens: Array<Token>) {
     this.tokens = tokens;
   }
 
   private expression(): Expr {
-    
+    return this.equality();
   }
 
+  private equality(): Expr {
+    let expr = this.comparison();
+
+    while (this.match(TokenType.BANG_EQUAL, TokenType.BANG_EQUAL)) {
+      const operator = this.previous();
+      const right = this.comparison();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  private match(...types: TokenType[]): boolean {
+    for (const tokentype of types) {
+      if (this.check(tokentype)) {
+        this.advance();
+        return true;
+      }
+    }
+    return false;
+  }
+  private check(tokentype: TokenType): boolean {
+    if(this.isAtEnd()) {
+        return false;
+    }
+    return typeof this.peek() === tokentype;
+  }
 }
