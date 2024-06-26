acea90b0528ad090b85a62fa97941e2c6286523e
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-06-27 00:27:39 +0530

    src: added some auxillary methods

--- src/Scanner.ts
+++ src/Scanner.ts
@@ -1,3 +1,4 @@
+import Lox from "./lox";
 import Token from "./token";
 import TokenType from "./tokentype";
 
@@ -27,7 +28,81 @@ class Scanner {
     return this.current >= this.source.length;
   }
 
-  private;
+  private scanTokens() {
+    const c = this.advance();
+    switch (c) {
+      case "(":
+        this.addToken(TokenType.LEFT_PAREN, null);
+        break;
+      case ")":
+        this.addToken(TokenType.RIGHT_PAREN, null);
+        break;
+      case "{":
+        this.addToken(TokenType.LEFT_BRACE, null);
+        break;
+      case "}":
+        this.addToken(TokenType.RIGHT_BRACE, null);
+        break;
+      case ",":
+        this.addToken(TokenType.COMMA, null);
+        break;
+      case ".":
+        this.addToken(TokenType.DOT, null);
+        break;
+      case "-":
+        this.addToken(TokenType.MINUS, null);
+        break;
+      case "+":
+        this.addToken(TokenType.PLUS, null);
+        break;
+      case ";":
+        this.addToken(TokenType.SEMICOLON, null);
+        break;
+      case "*":
+        this.addToken(TokenType.STAR, null);
+        break;
+      case "!":
+        this.addToken(this.match("=") ? TokenType.BANG_EQUAL : TokenType.BANG);
+        break;
+      case "=":
+        this.addToken(
+          this.match("=") ? TokenType.EQUAL_EQUAL : TokenType.EQUAL
+        );
+        break;
+      case "<":
+        this.addToken(this.match("=") ? TokenType.LESS_EQUAL : TokenType.LESS);
+        break;
+      case ">":
+        this.addToken(
+          this.match("=") ? TokenType.GREATER_EQUAL : TokenType.GREATER
+        );
+        break;
+
+      default:
+        Lox.error(this.line, "Unexpected character.");
+        break;
+    }
+  }
+
+  private advance(): string {
+    return this.source.charAt(this.current++);
+  }
+
+  private addToken(type: TokenType, literal: Object | null) {
+    const text = this.source.substring(this.start, this.current);
+    this.tokens.push(new Token(type, text, literal!, this.line));
+  }
+
+  private match(expected: string): boolean {
+    if (this.isAtEnd()) {
+      return false;
+    }
+    if (this.source.charAt(this.current) !== expected) {
+      return false;
+    }
+    this.current++;
+    return true;
+  }
 }
 
 export default Scanner;
