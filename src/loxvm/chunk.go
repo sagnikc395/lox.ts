61e1a01372e95aebe3480337b0e520cd3b849c7b
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-06-28 23:28:55 +0530

    src: start with the vm

---
+++ src/loxvm/chunk.go
@@ -0,0 +1,33 @@
+package main
+
+type OpCode = int
+
+const (
+	OP_RETURN OpCode = iota
+)
+
+type Chunk struct {
+	code     []uint8
+	count    int
+	capacity int
+}
+
+func InitChunk() *Chunk {
+	intialCapacity := 8 //8 bytes of memory
+	return &Chunk{
+		code:     make([]uint8, 0, intialCapacity),
+		capacity: 0,
+		count:    0,
+	}
+}
+
+func (c *Chunk) writeChunk(b uint8) {
+	if c.capacity < c.count+1 {
+		oldcapacity := c.capacity
+		c.code = append(c.code, uint8(c.capacity))
+		c.capacity = oldcapacity
+	}
+
+	c.code[c.count] = b
+	c.count++
+}
---
+++ src/loxvm/go.mod
@@ -0,0 +1,3 @@
+module github.com/sagnikc395/loxvm.git
+
+go 1.22.3
