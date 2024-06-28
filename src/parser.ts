03bd21eb31323dafe1ed9092773e8a127b03d6de
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-06-28 10:49:25 +0530

    src: added auxillary method

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,4 @@
-import { Expr } from "./expression";
+import { Binary, Expr } from "./expression";
 import Token from "./token";
 import TokenType from "./tokentype";
 
@@ -41,5 +41,39 @@ export class Parser {
       }
     }
     return false;
+  }
+
+  private synchronize() {
+    this.advance();
+    while (!this.isAtEnd()) {
+      if (this.previous().type === TokenType.SEMICOLON) return;
+
+      switch (this.peek().type) {
+        case TokenType.CLASS:
+        case TokenType.FUN:
+        case TokenType.VAR:
+        case TokenType.FOR:
+        case TokenType.IF:
+        case TokenType.WHILE:
+        case TokenType.PRINT:
+        case TokenType.RETURN:
+          return;
+      }
+      this.advance();
+    }
+  }
+
+  private equality(): Expr {
+    let expr = this.comparison();
+    while (this.match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
+      const operator = this.previous();
+      const right = this.comparison();
+      expr = new Binary(expr, right, operator);
+    }
+    return expr;
+  }
+
+  private expression(): Expr {
+    return this.equality();
   }
 }
