a08c3828584e1b78e71aa35f5b345009db48b2ec
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-06-28 11:05:42 +0530

    src: added more methods

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,5 @@
-import { Binary, Expr } from "./expression";
+import { Binary, Call, Expr, Literal, Unary, Variable } from "./expression";
+import Lox from "./lox";
 import Token from "./token";
 import TokenType from "./tokentype";
 
@@ -106,5 +107,78 @@ export class Parser {
     return expr;
   }
 
-  
+  private factor(): Expr {
+    let expr = this.unary();
+    while (this.match(TokenType.SLASH, TokenType.STAR)) {
+      const operator = this.previous();
+      const right = this.unary();
+      expr = new Binary(expr, right, operator);
+    }
+    return expr;
+  }
+
+  private unary(): Expr {
+    if (this.match(TokenType.BANG, TokenType.MINUS)) {
+      const operator = this.previous();
+      const right = this.unary();
+      return new Unary(right, operator);
+    }
+    return this.call();
+  }
+
+  private call(): Expr {
+    let expr = this.primary();
+    while (true) {
+      if (this.match(TokenType.LEFT_PAREN)) {
+        expr = this.finishCall(expr);
+      } else break;
+    }
+    return expr;
+  }
+
+  private finishCall(callee: Expr) {
+    const args: Expr[] = [];
+    if (!this.check(TokenType.RIGHT_PAREN)) {
+      do {
+        if (args.length > 255) {
+          this.error(this.peek(), "Can't have more than 255 arguments!");
+        }
+        args.push(this.expression());
+      } while (this.match(TokenType.COMMA));
+    }
+    const paren = this.consume(
+      TokenType.RIGHT_PAREN,
+      "Expect ')' after arguments."
+    );
+
+    return new Call(paren, callee, args);
+  }
+
+  private primary(): Expr {
+    if (this.match(TokenType.FALSE)) return new Literal(false);
+    if (this.match(TokenType.TRUE)) return new Literal(true);
+    if (this.match(TokenType.NIL)) return new Literal(null);
+    if (this.match(TokenType.NUMBER, TokenType.STRING))
+      return new Literal(this.previous().literal);
+    if (this.match(TokenType.IDENTIFIER)) {
+      return new Variable(this.previous());
+    }
+
+    if (this.match(TokenType.LEFT_PAREN)) {
+      const expr = this.expression();
+      this.consume(TokenType.RIGHT_PAREN, `Expect ")" after expression.`);
+      return new Grouping(expr);
+    }
+
+    throw this.error(this.peek(), "Expect expression.");
+  }
+  private consume(type: TokenType, message: string) {
+    if (this.check(type)) return this.advance();
+    throw this.error(this.peek(), message);
+  }
+
+  private error(token: Token, message: string) {
+    Lox.errorWithToken(token, message);
+    return new ParserError();
+  }
 }
